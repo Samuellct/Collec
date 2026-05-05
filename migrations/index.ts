437a0100490183dd47f7085f1ec88838ef37bd3a
Samuellct <samuel.lecomte37@gmail.com>
2026-05-05 14:03:08 +0200

    fix(migrations): add linked_pathway_id to collections, register all migrations
    - New migration adds linked_pathway_id FK on collections (was missing)
    - Register 20260425 and 20260502_100748 in migrations/index.ts
    - Use http-get:// in wait-on to accept any HTTP status as ready

--- migrations/index.ts
+++ migrations/index.ts
@@ -7,11 +7,13 @@ import * as migration_20260419_165648_add_release_date_to_media_items from './20
 import * as migration_20260419_180857_add_collections from './20260419_180857_add_collections';
 import * as migration_20260419_180940_add_collection_items from './20260419_180940_add_collection_items';
 import * as migration_20260424_100601_add_pathways_and_progression from './20260424_100601_add_pathways_and_progression';
+import * as migration_20260425_000000_add_linked_pathway_to_collections from './20260425_000000_add_linked_pathway_to_collections';
 import * as migration_20260426_100000_add_disabled_to_customers from './20260426_100000_add_disabled_to_customers';
 import * as migration_20260426_100100_add_pseudo_to_customers from './20260426_100100_add_pseudo_to_customers';
 import * as migration_20260427_130833_add_director_cast_to_media_items from './20260427_130833_add_director_cast_to_media_items';
 import * as migration_20260427_131000_add_fts_indexes_to_media_items from './20260427_131000_add_fts_indexes_to_media_items';
 import * as migration_20260428_add_slug_to_media_items from './20260428_add_slug_to_media_items';
+import * as migration_20260502_100748 from './20260502_100748';
 
 export const migrations = [
   {
@@ -60,6 +62,11 @@ export const migrations = [
     name: '20260424_100601_add_pathways_and_progression',
   },
   {
+    up: migration_20260425_000000_add_linked_pathway_to_collections.up,
+    down: migration_20260425_000000_add_linked_pathway_to_collections.down,
+    name: '20260425_000000_add_linked_pathway_to_collections',
+  },
+  {
     up: migration_20260426_100000_add_disabled_to_customers.up,
     down: migration_20260426_100000_add_disabled_to_customers.down,
     name: '20260426_100000_add_disabled_to_customers',
@@ -83,5 +90,10 @@ export const migrations = [
     up: migration_20260428_add_slug_to_media_items.up,
     down: migration_20260428_add_slug_to_media_items.down,
     name: '20260428_add_slug_to_media_items',
+  },
+  {
+    up: migration_20260502_100748.up,
+    down: migration_20260502_100748.down,
+    name: '20260502_100748',
   },
 ];
